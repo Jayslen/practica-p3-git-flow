c52abf83daaee1d104cf896b547c85d6eb5c3abf
jayslen <jayslen134@gmail.com>
2025-07-27 17:07:27 -0400

    feat: function to show the tasks added

--- task_cli.py
+++ task_cli.py
@@ -45,5 +45,37 @@ class MyCLI(cmd.Cmd):
             "You can perform certaint actions with these commands \n help: Get all the commands \n add: Add a task \n list: show all task saved \n list done: show all done tasks \n list in progress: show all task in progress \n list todo: show task to do \n mark_done {id}: mark the task selected as done \n mark_in_progress {id}:mark the task selected as in progress \n delete {id} delete a task providing the id of it \n update {id} new name: Edit a taks"
         )
 
+    def do_list(self, line):
+        tasks_to_show = []
+        task_txt = ""
+        if line.strip() not in tasks_status or len(line.strip()) == 0:
+            print("Showing all the tasks")
+            tasks_to_show = self.tasks
+        else:
+            tasks_to_show = [x for x in self.tasks if x["status"] == line]
+            task_txt = line
+
+        if len(self.tasks) == 0:
+            print("No tasks to show")
+            return None
+
+        rows = [list(x.values()) for x in tasks_to_show]
+        columns = list(self.tasks[0].keys())
+
+        table = Table(title=f"Tasks {task_txt}", show_lines=True, box=box.DOUBLE_EDGE)
+        console = Console()
+
+        for x in columns:
+            table.add_column(x)
+
+        for x in rows:
+            row = list(x)
+            row[0] = str(row[0])
+            row[1] = row[1].capitalize()
+            row[2] = row[2].capitalize()
+            table.add_row(*row, style="magenta")
+
+        console.print(table)
+
 if __name__ == "__main__":
     MyCLI().cmdloop()
